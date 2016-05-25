746ea42f2bd68c83e0315ea7e3c94591f0b89481
Christoph Anderson <cwolfanderson@gmail.com>
2016-05-24 18:32:26 -0700

    Added list view using an adapter

--- app/src/main/java/com/anderson/christoph/whatshouldwewatch/MainActivityFragment.java
+++ app/src/main/java/com/anderson/christoph/whatshouldwewatch/MainActivityFragment.java
@@ -5,11 +5,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    ArrayAdapter<String> movieAdapter;
 
     public MainActivityFragment() {
     }
@@ -17,6 +25,33 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] data = {
+                "Jaws",
+                "The Avengers",
+                "Some other movie",
+                "Something else",
+                "Making a Murderer",
+                "Who cares",
+                "Whatever",
+                "Enough?"
+        };
+
+
+        List<String> movies = new ArrayList<>(Arrays.asList(data));
+
+        movieAdapter = new ArrayAdapter<String>(
+                getActivity(), // the current context (this activity)
+                R.layout.list_item_movie, // the name of the layout ID
+                R.id.list_item_movie_textview, // the ID of the textview to populate
+                movies); // movie data
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        // Get a reference to the ListView, and attach this adapter to it.
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_movie);
+        listView.setAdapter(movieAdapter);
+
+        return rootView;
     }
 }
