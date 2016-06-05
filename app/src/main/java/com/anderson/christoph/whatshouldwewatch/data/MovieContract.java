d7bdd10c8539495042787a0e75c2e8443180e5f1
Christoph Anderson <cwolfanderson@gmail.com>
2016-06-04 19:02:05 -0700

    Finished db stuff

--- app/src/main/java/com/anderson/christoph/whatshouldwewatch/data/MovieContract.java
+++ app/src/main/java/com/anderson/christoph/whatshouldwewatch/data/MovieContract.java
@@ -2,12 +2,8 @@ package com.anderson.christoph.whatshouldwewatch.data;
 
 import android.provider.BaseColumns;
 
-/**
- * Defines table and column names for the weather database.
- */
 public class MovieContract {
 
-    /* Inner class that defines the table contents of the weather table */
     public static final class Review implements BaseColumns {
 
         public static final String TABLE_NAME = "reviews";
@@ -16,31 +12,5 @@ public class MovieContract {
         public static final String COLUMN_SUMMARY_SHORT = "overview";
         public static final String COLUMN_PUBLISH_DATE = "release_date";
 
-//        // Column with the foreign key into the location table.
-//        public static final String COLUMN_LOC_KEY = "location_id";
-//        // Date, stored as long in milliseconds since the epoch
-//        public static final String COLUMN_DATE = "date";
-//        // Weather id as returned by API, to identify the icon to be used
-//        public static final String COLUMN_WEATHER_ID = "weather_id";
-//
-//        // Short description and long description of the weather, as provided by API.
-//        // e.g "clear" vs "sky is clear".
-//        public static final String COLUMN_SHORT_DESC = "short_desc";
-//
-//        // Min and max temperatures for the day (stored as floats)
-//        public static final String COLUMN_MIN_TEMP = "min";
-//        public static final String COLUMN_MAX_TEMP = "max";
-//
-//        // Humidity is stored as a float representing percentage
-//        public static final String COLUMN_HUMIDITY = "humidity";
-//
-//        // Humidity is stored as a float representing percentage
-//        public static final String COLUMN_PRESSURE = "pressure";
-//
-//        // Windspeed is stored as a float representing windspeed  mph
-//        public static final String COLUMN_WIND_SPEED = "wind";
-//
-//        // Degrees are meteorological degrees (e.g, 0 is north, 180 is south).  Stored as floats.
-//        public static final String COLUMN_DEGREES = "degrees";
     }
 }
--- app/src/main/java/com/anderson/christoph/whatshouldwewatch/data/MovieDbHelper.java
+++ app/src/main/java/com/anderson/christoph/whatshouldwewatch/data/MovieDbHelper.java
@@ -13,8 +13,7 @@ import com.anderson.christoph.whatshouldwewatch.data.MovieContract.Review;
  */
 public class MovieDbHelper extends SQLiteOpenHelper {
 
-    // If you change the database schema, you must increment the database version.
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 1;
 
     static final String DATABASE_NAME = "movie.db";
 
@@ -25,38 +24,14 @@ public class MovieDbHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         final String SQL_CREATE_MOVIE_TABLE = "CREATE TABLE " + Review.TABLE_NAME + " (" +
-                // Unique keys will be auto-generated in either case.  But for movie
-                // reviews, it's reasonable to assume the user will want information
-                // for a certain date and all dates *following*, so the forecast data
-                // should be sorted accordingly.
+
                 Review._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
 
-                Review.COLUMN_SUMMARY_SHORT + "REAL NOT NULL, " +
-                Review.COLUMN_PUBLISH_DATE + "REAL NOT NULL, " +
+                Review.COLUMN_DISPLAY_TITLE + " REAL NOT NULL, " +
+                Review.COLUMN_SUMMARY_SHORT + " REAL NOT NULL, " +
+                Review.COLUMN_PUBLISH_DATE + " REAL NOT NULL;";
 
-                // the ID of the location entry associated with this weather data
-                Review.COLUMN_DISPLAY_TITLE + " INTEGER NOT NULL;";
 
-//                Review.COLUMN_DATE + " INTEGER NOT NULL, " +
-//                Review.COLUMN_SHORT_DESC + " TEXT NOT NULL, " +
-//                Review.COLUMN_WEATHER_ID + " INTEGER NOT NULL," +
-//
-//                Review.COLUMN_MIN_TEMP + " REAL NOT NULL, " +
-//                Review.COLUMN_MAX_TEMP + " REAL NOT NULL, " +
-//
-//                Review.COLUMN_HUMIDITY + " REAL NOT NULL, " +
-//                Review.COLUMN_PRESSURE + " REAL NOT NULL, " +
-//                Review.COLUMN_WIND_SPEED + " REAL NOT NULL, " +
-//                Review.COLUMN_DEGREES + " REAL NOT NULL, " +
-
-//                // Set up the location column as a foreign key to location table.
-//                " FOREIGN KEY (" + Review.COLUMN_LOC_KEY + ") REFERENCES " +
-//                LocationEntry.TABLE_NAME + " (" + LocationEntry._ID + "), " +
-
-                // To assure the application have just one weather entry per day
-                // per location, it's created a UNIQUE constraint with REPLACE strategy
-//                " UNIQUE (" + Review.COLUMN_DATE + ", " +
-//                Review.COLUMN_LOC_KEY + ") ON CONFLICT REPLACE);";
 
         sqLiteDatabase.execSQL(SQL_CREATE_MOVIE_TABLE);
     }
@@ -69,7 +44,6 @@ public class MovieDbHelper extends SQLiteOpenHelper {
         // It does NOT depend on the version number for your application.
         // If you want to update the schema without wiping data, commenting out the next 2 lines
         // should be your top priority before modifying this method.
-//        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + LocationEntry.TABLE_NAME);
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + Review.TABLE_NAME);
         onCreate(sqLiteDatabase);
     }
